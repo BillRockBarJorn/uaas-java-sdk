4a9557ac680f4ea516a1200cb8d6ff8ae6e4bef8
wuzz <wuzz@heredata.com.cn>
2023-09-27 14:00:28 +0800

    添加测试SDK模块，可以通过java命令行直接调用SDK

---
+++ java-sdk-test/src/main/java/com/heredata/MainTest.java
@@ -0,0 +1,19 @@
+package com.heredata;
+
+/**
+ * TODO
+ * @author wuzz
+ * @since 2023/5/19
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+
+///home/sm_shuhaiwen/ncoss/jdk8
+    }
+
+}
